ac57044e5c97b7124a5ffcca449c18abe65c1904
Tjarda112 <tjardavoorn@hotmail.com>
2019-03-14 13:46:23 +0100

    change database

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig= {
   // Initialize Firebase
     fire:{
-    apiKey: "AIzaSyCejwGipJ2n-lwh_8RlRNvVe0a2OWKThzQ",
-    authDomain: "ionicproject-51dae.firebaseapp.com",
-    databaseURL: "https://ionicproject-51dae.firebaseio.com",
-    projectId: "ionicproject-51dae",
-    storageBucket: "ionicproject-51dae.appspot.com",
-    messagingSenderId: "666593995762"
+      apiKey: "AIzaSyCo1FpHVgI3f0LeOEGTKNwXguTgFHeNf1w",
+      authDomain: "ictlabsite-28206.firebaseapp.com",
+      databaseURL: "https://ictlabsite-28206.firebaseio.com",
+      projectId: "ictlabsite-28206",
+      storageBucket: "ictlabsite-28206.appspot.com",
+      messagingSenderId: "994027048597"
 }};
