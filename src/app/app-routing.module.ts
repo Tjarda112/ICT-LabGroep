cb8575695366abb4ce5180b925b442302ae9316d
Tjarda112 <tjardavoorn@hotmail.com>
2019-03-14 13:43:06 +0100

    Add files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'upload', loadChildren: './upload/upload.module#UploadPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { Platform,MenuController,NavController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  rootPage: any;
+
+  public appMenu = [
+    {title: 'Home', url: '/home', icon: 'list'},
+    {title: 'Bestanden', url: '/upload', icon: 'add'},
+
+  ]
+  @ViewChild(NavController) nav: NavController;
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private auth: AuthService,
+    private statusBar: StatusBar,
+    private menu: MenuController,
+    private router: Router,
+    private navCtrl: NavController
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+    });
+  
+    this.auth.afAuth.authState
+      .subscribe(
+        user => {
+          if (user) {
+            this.rootPage = this.navCtrl.navigateRoot('/home');
+          } else {
+            this.rootPage = this.navCtrl.navigateRoot('/login');
+          }
+        },
+        () => {
+          this.rootPage = this.navCtrl.navigateRoot('/login');
+        }
+      );
+  }
+  login() {
+    this.menu.close();
+    this.auth.signOut();
+    this.navCtrl.navigateRoot('/login')
+  }
+  logout() {
+    this.menu.close();
+    this.auth.signOut();
+    this.navCtrl.navigateRoot('/home')
+
+  } 
+}
